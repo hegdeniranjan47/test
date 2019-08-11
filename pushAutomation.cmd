2f1650d090f31bf019493f5446b14290547a5601
Niranjan <niranjan.hegde@tektronix.com>
2019-08-11 09:56:01 +0530

    Sun 08/11/2019 9:56:01.18

--- pushAutomation.cmd
+++ pushAutomation.cmd
@@ -1,6 +1,4 @@
-echo "# test" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git remote add origin https://github.com/hegdeniranjan47/test.git
-git push -u origin master
+git add -u
+git commit -m "%date%%time%"
+git push origin master
+pause
